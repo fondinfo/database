e666da5c74ed42ac7e8f3c974ed8aaca3cc7b105
Alberto Ferrari <alberto.ferrari@unipr.it>
2023-09-13 22:52:11 +0200

    Update university-ql.sql

--- university-ql.sql
+++ university-ql.sql
@@ -1,14 +1,18 @@
+-- First name and last name of of students born after the year 2000
 SELECT FirstName, LastName
 FROM Student
 WHERE YearOfBirth > 2000;
 
+-- All information relating to teachers
 SELECT *
 FROM Teacher;
 
+-- All information relating to teachers and courses
 SELECT *
 FROM Teacher 
 INNER JOIN Course ON TeacherID = CourseTeacher;
 
+-- Firs name and last name of the students with their courses and their teachers
 SELECT Student.FirstName, Student.LastName, Course.CourseName, Teacher.LastName
 FROM Student 
 	INNER JOIN Exam ON Student.StudentID = Exam.StudentID
